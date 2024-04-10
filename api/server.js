0e33b336102d779d2ea4422e5d50b6ecaea69754
Mariam Hussein <mariamh99@gmail.com>
2024-04-10 18:22:57 +0300

    server.js changes

--- api/server.js
+++ api/server.js
@@ -10,17 +10,37 @@ import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const app = express();
 dotenv.config();
 mongoose.set("strictQuery", true);
 
 const connect = async () => {
+  const uri = process.env.MONGO;
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
   try {
-    await mongoose.connect('mongodb+srv://capstoneproj:a00039_capstone@cluster0.8qfqugs.mongodb.net/almaerid?retryWrites=true&w=majority&appName=Cluster0');
-    console.log("Connected to mongoDB!");
+    // Connect the client to the server
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (error) {
-    console.log(error);
+    console.error('Failed to connect to MongoDB', error);
+    process.exit(1);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 };
 
@@ -43,7 +63,18 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(8800, () => {
-  connect();
-  console.log("Backend server is running!");
-});
+const startServer = async () => {
+  try {
+    await connect();
+    app.listen(8800, () => {
+      console.log("Backend server is running!");
+    });
+  } catch (error) {
+    console.error('Failed to start server', error);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
